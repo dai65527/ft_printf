7ad6f5b6c5c330cae4a143aa70363f0c96f25f4b
dnakano <dnakano@student.42tokyo.jp>
2020-10-12 10:36:55 +0900

    Modify: ft_printf_putpadding.c (make it not to put 0 padding when left adjustment)

--- ft_printf_putpadding.c
+++ ft_printf_putpadding.c
@@ -6,7 +6,7 @@
 /*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/11 09:02:48 by dnakano           #+#    #+#             */
-/*   Updated: 2020/10/11 09:03:32 by dnakano          ###   ########.fr       */
+/*   Updated: 2020/10/12 10:34:16 by dnakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void			ft_printf_putpadding(int width, t_printf_flags *flags)
 {
 	while (width > 0)
 	{
-		if (flags->flag & FLAG_ZEROPADDING)
+		if (flags->flag & FLAG_ZEROPADDING && !(flags->flag & FLAG_LEFTADJUST))
 			write(1, "0", 1);
 		else
 			write(1, " ", 1);
