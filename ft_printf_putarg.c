778e3146ad58fb3b9f1969dd9c01f4be9995d193
dnakano <dnakano@student.42tokyo.jp>
2020-10-12 17:03:59 +0900

    Modify: ft_printf_putarg.c (fixed when no format chars)

--- ft_printf_putarg.c
+++ ft_printf_putarg.c
@@ -6,7 +6,7 @@
 /*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/11 09:04:45 by dnakano           #+#    #+#             */
-/*   Updated: 2020/10/12 15:56:31 by dnakano          ###   ########.fr       */
+/*   Updated: 2020/10/12 17:00:22 by dnakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,7 @@
 
 static int		ft_printf_putarg_put(const char fc, va_list ap, t_printf_flags *flags)
 {
-	if (!fc)
-		return (0);
-	else if (fc == '%')
+	if (fc == '%')
 		return (ft_printf_putpercent(ap, flags));
 	else if (fc == 'd' || fc == 'i' || fc == 'u' || fc == 'x' || fc == 'X')
 		return (ft_printf_putint(fc, ap, flags));
@@ -28,16 +26,18 @@ static int		ft_printf_putarg_put(const char fc, va_list ap, t_printf_flags *flag
 		return (ft_printf_putbyte(ap, flags));
 	else if (fc == 's')
 		return (ft_printf_putstr(ap, flags));
-	return (1);
+	return (-1);
 }
 
 char			*ft_printf_putarg(const char *format, va_list ap, int *count)
 {
+	int				res;
 	t_printf_flags	flags;
 
 	format = ft_printf_findflags(format, &flags);
-	*count += ft_printf_putarg_put(*format, ap, &flags);
-	if (*format)
-		format++;
-	return ((char *)format);
+	res = ft_printf_putarg_put(*format, ap, &flags);
+	if (res == -1)
+		return ((char *)format);
+	*count += res;
+	return ((char *)format + 1);
 }
